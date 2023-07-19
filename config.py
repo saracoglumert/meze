fa36ca55d8dd76d6e12dda44d17efd0b4e2b38cb
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2023-07-19 16:50:07 +0200

    folder structure

---
+++ config.py
@@ -0,0 +1,9 @@
+CONST_FREQS         = ['D','W','M']
+CONST_SAMPLE_RANGE_HIGH  = range(1,1024)
+CONST_SAMPLE_RANGE_MID = range(1,128)
+CONST_SAMPLE_RANGE_LOW = range(1,8)
+CONST_SAMPLE_NAME   = 4
+
+ERROR_LENGTH = "Length cannot be non-integer."
+ERROR_OBJTYPE = "Type is not correct." 
+ERROR_FILETYPE = "Filetype is unknown."
--- meze.py
+++ meze.py
@@ -1,3 +1,5 @@
+import config
+
 import pandas as pd
 import numpy as np
 import string
@@ -5,15 +7,7 @@ import random
 import datetime
 import pickle
 
-CONST_FREQS         = ['D','W','M']
-CONST_SAMPLE_RANGE_HIGH  = range(1,1024)
-CONST_SAMPLE_RANGE_MID = range(1,128)
-CONST_SAMPLE_RANGE_LOW = range(1,8)
-CONST_SAMPLE_NAME   = 4
 
-ERROR_LENGTH = "Length cannot be non-integer."
-ERROR_OBJTYPE = "Type is not correct." 
-ERROR_FILETYPE = "Filetype is unknown."
 
 class Tools:
     @staticmethod
